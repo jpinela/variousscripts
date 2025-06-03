43e48f22a4847e9c9ad5cfab21430ab8ba87b9ef
JP <>
2025-06-03 22:29:01 +0100

    added p5.js vscode

---
+++ processing/p5js/sketch.js
@@ -0,0 +1,87 @@
+
+function drawGrid(){
+  let numRows = 10;
+  let numCols = 10;
+  let unitPixels = 40;
+  
+  //https://p5js.org/examples/animation-and-variables-drawing-lines/
+  stroke(0,0,0);
+  strokeWeight(2);
+  for (let i=0;i<numRows;i++){ //desenhar linhas
+    line(30, 100+unitPixels*i, 30+numCols*unitPixels, 100+unitPixels*i);     
+
+  }
+  for (let j=0;j<numCols;j++){ //desenhar barras
+    line(30+unitPixels*j, 100, 30+unitPixels*j, 100+unitPixels*numRows); 
+ 
+  }
+  strokeWeight(1);
+}
+
+function drawRobot1(ang1,ang2){
+  push();
+  rotate(ang1);
+  line(30,100,30+20*4,100);
+  pop();
+}
+
+function drawRobot2(ang1degs,ang2degs,ang3degs){
+  let arm1l=4, arm2l=3, arm3l=2;
+  strokeWeight(4);
+  stroke(255,0,0);
+  let ang1=ang1degs*PI/180, ang2=ang2degs*PI/180 , ang3=ang3degs*PI/180 ;
+  stroke(255,0,0);
+  
+  let pointarm1x=30+40*arm1l*cos(ang1);
+  let pointarm1y=100+40*arm1l*sin(ang1);
+  line(30,100,pointarm1x,pointarm1y);
+
+  let pointarm2x=pointarm1x+40*arm2l*cos(ang2);
+  let pointarm2y=pointarm1y+40*arm2l*sin(ang2);
+  line(pointarm1x,pointarm1y,pointarm2x,pointarm2y);
+
+  let pointarm3x=pointarm2x+40*arm3l*cos(ang3);
+  let pointarm3y=pointarm2y+40*arm3l*sin(ang3);
+  line(pointarm2x,pointarm2y,pointarm3x,pointarm3y);
+
+  stroke(255,255,255);
+  strokeWeight(1);
+}
+
+function setup() {
+  createCanvas(600, 600);
+
+  //https://editor.p5js.org/gr.gromko/sketches/f-oF3Cf0q
+  slider = createSlider(0, 180, 90); //повзунок, значення 0-255
+  slider.position(20, 15);  //позиція
+  slider.style('width', '200px'); // стиль
+
+  slider2 = createSlider(0, 180, 90); //повзунок, значення 0-255
+  slider2.position(20, 35);  //позиція
+  slider2.style('width', '200px'); // стиль
+
+  slider3 = createSlider(0, 180, 90); //повзунок, значення 0-255
+  slider3.position(20, 55);  //позиція
+  slider3.style('width', '200px'); // стиль
+
+  background("aqua");
+  drawGrid();
+
+}
+
+function draw() {
+
+  background("aqua");
+  //https://p5js.org/examples/transformation-rotate/
+  // rotate / push / pop. curioso!
+  let val = slider.value();
+  let val2 = slider2.value();
+  let val3 = slider3.value();
+
+  text(val,0,30);
+  text(val2,0,50);
+  text(val3,0,70);
+
+  drawGrid();
+  drawRobot2(val,val2,val3);
+}
